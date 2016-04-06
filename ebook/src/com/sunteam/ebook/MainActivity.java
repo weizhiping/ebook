b6961cb8c4c45678324dcb0b071e8e9278e84119
wzp <wzp1029@126.com>
2016-04-06 11:22:52 +0800

    修改主界面布局。

--- ebook/src/com/sunteam/ebook/MainActivity.java
+++ ebook/src/com/sunteam/ebook/MainActivity.java
@@ -2,13 +2,12 @@ package com.sunteam.ebook;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity 
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
