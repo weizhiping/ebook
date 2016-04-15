9ea23e7c416c767128dea8e712045b3d65f258e0
wzp <wzp1029@126.com>
2016-04-15 10:55:40 +0800

    优化代码。

--- ebook/src/com/sunteam/ebook/ReadTxtActivity.java
+++ ebook/src/com/sunteam/ebook/ReadTxtActivity.java
@@ -1,6 +1,7 @@
 package com.sunteam.ebook;
 
 import com.sunteam.ebook.util.EbookConstants;
+
 import com.sunteam.ebook.util.PublicUtils;
 import com.sunteam.ebook.util.TextFileReaderUtils;
 import com.sunteam.ebook.view.TextReaderView;
@@ -11,12 +12,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
 /**
  * TXT文件显示
+ * 
  * @author sylar
- *
  */
-public class ReadTxtActivity extends Activity implements OnPageFlingListener {
+public class ReadTxtActivity extends Activity implements OnPageFlingListener 
+{
 	private TextView mTvTitle = null;
 	private TextView mTvPageCount = null;
 	private TextView mTvCurPage = null;
@@ -32,8 +35,7 @@ public class ReadTxtActivity extends Activity implements OnPageFlingListener {
 		String filename = getIntent().getStringExtra("name");
 		int part = getIntent().getIntExtra("part", 0);
 		
-		//此处需要从系统配置文件中得到配色方案索引
-    	PublicUtils.setColorSchemeIndex(mColorSchemeIndex);
+		mColorSchemeIndex = PublicUtils.getColorSchemeIndex();
     	this.getWindow().setBackgroundDrawableResource(EbookConstants.ViewBkDrawable[mColorSchemeIndex]);
     	mTvTitle = (TextView)this.findViewById(R.id.main_title);
     	mTvPageCount = (TextView)this.findViewById(R.id.pageCount);
@@ -54,25 +56,29 @@ public class ReadTxtActivity extends Activity implements OnPageFlingListener {
 	}
 
 	@Override
-	public void onPageFlingToTop() {
+	public void onPageFlingToTop() 
+	{
 		// TODO Auto-generated method stub
 		Toast.makeText(this, "已经是第一页了！", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
-	public void onPageFlingToBottom() {
+	public void onPageFlingToBottom() 
+	{
 		// TODO Auto-generated method stub
 		Toast.makeText(this, "已经是最后一页了！", Toast.LENGTH_LONG).show();
 	}
 
 	@Override
-	public void onPageFlingCompleted(int curPage) {
+	public void onPageFlingCompleted(int curPage) 
+	{
 		// TODO Auto-generated method stub
 		mTvCurPage.setText(curPage+"");
 	}
 
 	@Override
-	public void onLoadCompleted(int pageCount, int curPage) {
+	public void onLoadCompleted(int pageCount, int curPage) 
+	{
 		// TODO Auto-generated method stub
 		mTvPageCount.setText(pageCount+"");
 		mTvCurPage.setText(curPage+"");
