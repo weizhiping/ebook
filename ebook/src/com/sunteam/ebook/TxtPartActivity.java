2f719d6e39c6eb53e54c110da27752c555d35a32
wzp <wzp1029@126.com>
2016-04-15 10:52:00 +0800

    优化代码。

--- ebook/src/com/sunteam/ebook/TxtPartActivity.java
+++ ebook/src/com/sunteam/ebook/TxtPartActivity.java
@@ -6,71 +6,78 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.view.View;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.FrameLayout;
 
-import com.sunteam.ebook.adapter.TxtDetailListAdapter;
-import com.sunteam.ebook.adapter.TxtPartListAdapter;
-import com.sunteam.ebook.util.EbookConstants;
-import com.sunteam.ebook.util.PublicUtils;
+import com.sunteam.ebook.adapter.MainListAdapter.OnEnterListener;
+import com.sunteam.ebook.view.MainView;
+
 /**
  * 文档部分列表界面
+ * 
  * @author sylar
- *
  */
-public class TxtPartActivity extends Activity {
-	private ListView mLvMenu = null;
-	private TxtDetailListAdapter mAdapter = null;
-	private int mColorSchemeIndex = 0;	//系统配色索引
+
+public class TxtPartActivity extends Activity implements OnEnterListener 
+{
+	private FrameLayout mFlContainer = null;
+	private MainView mMainView = null;
+	private ArrayList<String> mMenuList = null;
+	private String filename = null;
 	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_txt);
-	     initViews();
-	    }
-	    
-	    private void initViews()
-	    {
-	    	PublicUtils.setColorSchemeIndex(mColorSchemeIndex);
-	    	this.getWindow().setBackgroundDrawableResource(EbookConstants.ViewBkDrawable[mColorSchemeIndex]);
-	    	Intent intent = getIntent();
-	    	String name = intent.getStringExtra("name");
-	    	int count = intent.getIntExtra("count", 0);
-	    	TextView mTvTitle = (TextView)this.findViewById(R.id.txt_title);
-	    	mLvMenu = (ListView)this.findViewById(R.id.txt_list);
-	    	View mLine = (View)this.findViewById(R.id.line);
-	    	mTvTitle.setText(name);
-	    	mTvTitle.setTextColor(this.getResources().getColor(EbookConstants.FontColorID[mColorSchemeIndex]));
-	    	mLine.setBackgroundResource(EbookConstants.FontColorID[mColorSchemeIndex]);
-	    	mLvMenu.setFocusable(false);	//不让控件获得焦点，让主界面进行按键分发
-	    	
-	    	ArrayList<String> partList = new ArrayList<String>();
-	    	for(int i = 1; i <= count; i++){
-	    		partList.add("第" + i + "部分");
-	    	}
-	    	TxtPartListAdapter mAdapter = new TxtPartListAdapter( this, name, partList);
-	    	mLvMenu.setAdapter(mAdapter);
-	    }
+		setContentView(R.layout.activity_main);
+		
+		initViews();
+	}
 	
+	private void initViews()
+    {
+		Intent intent = getIntent();
+		filename = intent.getStringExtra("name");
+    	int count = intent.getIntExtra("count", 0);
+    	
+    	mMenuList = new ArrayList<String>();
+    	for( int i = 1; i <= count; i++ )
+    	{
+    		mMenuList.add(String.format(this.getResources().getString(R.string.txt_menu_part), i ));
+    	}
+    	
+    	mFlContainer = (FrameLayout)this.findViewById(R.id.fl_container);
+    	mMainView = new MainView( this, this, filename, mMenuList );
+    	mFlContainer.removeAllViews();
+    	mFlContainer.addView(mMainView.getView());
+    }
+ 
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) 
 	{
 		switch( keyCode )
 		{
 			case KeyEvent.KEYCODE_DPAD_UP:		//上
-				mAdapter.up();
+				mMainView.up();
 				return	true;
-			case KeyEvent.KEYCODE_DPAD_DOWN:	//下 
-				mAdapter.down();
+			case KeyEvent.KEYCODE_DPAD_DOWN:	//下
+				mMainView.down();
 				return	true;
 			case KeyEvent.KEYCODE_DPAD_CENTER:	//确定
-				mAdapter.enter();
+				mMainView.enter();
 				return	true;
 			default:
 				break;
 		}
 		return super.onKeyDown(keyCode, event);
-	}   
+	}
+
+	@Override
+	public void onEnterCompleted(int selectItem, String menu) 
+	{
+		// TODO Auto-generated method stub
+		Intent intent = new Intent(this,ReadTxtActivity.class);
+		intent.putExtra("name", filename);
+		intent.putExtra("part", selectItem);
+		this.startActivity(intent);
+	} 
 }
