dca6726f4ea38b7ae7914ef7d2a2ff5292388ed1
loujiawen <loujiawen924@163.com>
2016-05-16 14:35:11 +0800

    完善Diasy文档树形结构

--- ebook/src/com/sunteam/ebook/DaisyDetailActivity.java
+++ ebook/src/com/sunteam/ebook/DaisyDetailActivity.java
@@ -1,11 +1,11 @@
 package com.sunteam.ebook;
 
-import java.io.File;
 import java.util.ArrayList;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.widget.FrameLayout;
 import android.widget.Toast;
@@ -14,38 +14,45 @@ import com.sunteam.ebook.adapter.MainListAdapter.OnEnterListener;
 import com.sunteam.ebook.entity.DiasyNode;
 import com.sunteam.ebook.entity.FileInfo;
 import com.sunteam.ebook.util.DaisyFileReaderUtils;
-import com.sunteam.ebook.util.FileOperateUtils;
 import com.sunteam.ebook.view.MainView;
 
 /**
- * Daisy主界面
+ * Daisy索引界面
  * 
  * @author sylar
  */
-public class DaisyActivity extends Activity implements OnEnterListener {
+public class DaisyDetailActivity extends Activity implements OnEnterListener {
 	private FrameLayout mFlContainer = null;
 	private MainView mMainView = null;
 	private ArrayList<String> mMenuList = null;
-	private ArrayList<FileInfo> fileInfoList = null;
+	private ArrayList<DiasyNode> diasList;
 	private int catalog;// 1为txt文档，2为word文档,3为disay
 	private FileInfo remberFile;
+	private String path;
+	private int seq;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		catalog = getIntent().getIntExtra("catalogType", 0);
+		Intent intent = getIntent();
+		String name = intent.getStringExtra("name");
+		path = intent.getStringExtra("path");
+		catalog = intent.getIntExtra("catalogType", 0);
+		seq = intent.getIntExtra("seq", -1);
 		remberFile = (FileInfo) getIntent().getSerializableExtra("file");
-		initViews();
+		
+		diasList = (ArrayList<DiasyNode>) intent.getSerializableExtra("diasys");
+		Log.e("diasy", "-------seq---:" + diasList);
+		
+		initViews(name);
 	}
 
-	private void initViews() {
+	private void initViews(String name) {
 		mMenuList = new ArrayList<String>();
-		fileInfoList = new ArrayList<FileInfo>();
 		initFiles();
 		mFlContainer = (FrameLayout) this.findViewById(R.id.fl_container);
-		mMainView = new MainView(this, this,
-				this.getString(R.string.main_menu_daisy), mMenuList);
+		mMainView = new MainView(this, this,name, mMenuList);
 		mFlContainer.removeAllViews();
 		mFlContainer.addView(mMainView.getView());
 		if (null != remberFile) {
@@ -63,15 +70,9 @@ public class DaisyActivity extends Activity implements OnEnterListener {
 
 	// 初始化显示文件
 	private void initFiles() {
-		ArrayList<File> filesList = FileOperateUtils.getDaisyInDir();
-		if (null != filesList) {
-			for (File f : filesList) {
-				if (f.isDirectory()) {
-					mMenuList.add(f.getName());
-					FileInfo fileInfo = new FileInfo(f.getName(), f.getPath(), true,
-							catalog, 0, 0);
-					fileInfoList.add(fileInfo);
-				}
+		if (null != diasList) {
+			for (DiasyNode f : diasList) {
+				mMenuList.add(f.name);
 			}
 		}
 	}
@@ -97,22 +98,20 @@ public class DaisyActivity extends Activity implements OnEnterListener {
 
 	@Override
 	public void onEnterCompleted(int selectItem, String menu) {
-		
-		FileInfo file= fileInfoList.get(selectItem);
-		DaisyFileReaderUtils.getInstance().init(file.path + "/ncc.html");
-		ArrayList<DiasyNode> diasList = DaisyFileReaderUtils.getInstance().getChildNodeList(-1);
-		Intent intent = null;
-		if(null != diasList && diasList.size() > 0){
-			intent = new Intent(this, DaisyDetailActivity.class);
-			intent.putExtra("diasys", diasList);
-		}else{
+		DiasyNode dias = diasList.get(selectItem);
+		ArrayList<DiasyNode> diaysList = DaisyFileReaderUtils.getInstance().getChildNodeList(dias.seq);
+		int size = diaysList.size();
+		if(0 == size){
 			Toast.makeText(this, "-----需要进入阅读界面----", Toast.LENGTH_SHORT).show();
-			intent = new Intent(this, DaisyDetailActivity.class);
+		}else{
+			Intent intent = new Intent(this, DaisyDetailActivity.class);
+			intent.putExtra("name", menu);
+			intent.putExtra("seq", dias.seq);
+			intent.putExtra("catalogType", catalog);
+			intent.putExtra("path", path);
+			intent.putExtra("file", remberFile);
+			intent.putExtra("diasys",  diaysList);
+			this.startActivity(intent);
 		}
-		intent.putExtra("name", menu);
-		intent.putExtra("catalogType", file.catalog);
-		intent.putExtra("path", file.path);
-		intent.putExtra("file", remberFile);
-		this.startActivity(intent);
 	}
 }
--- ebook/src/com/sunteam/ebook/entity/DiasyNode.java
+++ ebook/src/com/sunteam/ebook/entity/DiasyNode.java
@@ -1,17 +1,24 @@
 package com.sunteam.ebook.entity;
 
+import java.io.Serializable;
+
 /**
  * Diasy 节点类
  * 
  * @author wzp
- *
+ * 
  */
-public class DiasyNode 
+public class DiasyNode implements Serializable
 {
-	public int father;		//父节点序号
-	public int seq;			//节点序号
-	public int level;		//节点等级
-	public String name;		//节点名称
-	public String href;		//子链接
-	public String label;	//标签
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7345178245843082550L;
+	
+	public int father; // 父节点序号
+	public int seq; // 节点序号
+	public int level; // 节点等级
+	public String name; // 节点名称
+	public String href; // 子链接
+	public String label; // 标签
 }
