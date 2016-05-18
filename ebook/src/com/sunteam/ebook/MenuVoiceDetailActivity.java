a785d9bba06c8876fa1fc32207851170f9f59e28
sylar <loujiawen924@163.com>
2016-05-18 18:10:27 +0800

    添加功能菜单朗读详情页面

--- ebook/src/com/sunteam/ebook/MenuVoiceDetailActivity.java
+++ ebook/src/com/sunteam/ebook/MenuVoiceDetailActivity.java
@@ -3,7 +3,6 @@ package com.sunteam.ebook;
 import java.util.ArrayList;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -13,32 +12,53 @@ import com.sunteam.ebook.adapter.MainListAdapter.OnEnterListener;
 import com.sunteam.ebook.view.MainView;
 
 /**
- * 朗读语音界面
+ * 朗读语音子界面界面
  * 
  * @author sylar
  */
-public class MenuVoiceActivity extends Activity implements OnEnterListener {
-	private static String TAG = "MenuVoiceActivity";
+public class MenuVoiceDetailActivity extends Activity implements
+		OnEnterListener {
+	private static String TAG = "MenuVoiceDetailActivity";
 	private FrameLayout mFlContainer = null;
 	private MainView mMainView = null;
 	private ArrayList<String> mMenuList = null;
+	private int voiceFlag;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		initData();
 		initViews();
 	}
-
-	private void initViews() {
+	
+	private void initData(){
+		voiceFlag = getIntent().getIntExtra("voice_flag", 0);
 		Resources res = getResources();
-		
-		String[] menus = res.getStringArray(R.array.array_menu_voice);
+		String[] menus = null;
+		switch (voiceFlag) {
+		case 0:
+			menus = res.getStringArray(R.array.array_menu_voice_china);
+			break;
+		case 1:
+			menus = res.getStringArray(R.array.array_menu_voice_english);
+			break;
+		case 4:
+			menus = res.getStringArray(R.array.array_menu_voice_style);
+			break;
+		case 5:
+			menus = res.getStringArray(R.array.array_menu_voice_effect);
+			break;
+		}
 		int length = menus.length;
 		mMenuList = new ArrayList<String>();
 		for (int i = 0; i < length; i++) {
 			mMenuList.add(menus[i]);
 		}
+	}
+
+	private void initViews() {
+		
 		String title = this.getString(R.string.menu_function);
 		mFlContainer = (FrameLayout) this.findViewById(R.id.fl_container);
 		mMainView = new MainView(this, this, title, mMenuList);
@@ -46,16 +66,14 @@ public class MenuVoiceActivity extends Activity implements OnEnterListener {
 		mFlContainer.addView(mMainView.getView());
 	}
 
-    @Override
-    public void onResume()
-    {
-    	if( mMainView != null )
-    	{
-    		mMainView.onResume();
-    	}
-    	super.onResume();
-    }
-    
+	@Override
+	public void onResume() {
+		if (mMainView != null) {
+			mMainView.onResume();
+		}
+		super.onResume();
+	}
+
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		switch (keyCode) {
@@ -81,30 +99,24 @@ public class MenuVoiceActivity extends Activity implements OnEnterListener {
 	@Override
 	public void onEnterCompleted(int selectItem, String menu) {
 		switch (selectItem) {
-		case 0://中文角色
-			startToDetail(0);
+		case 0:
+
 			break;
-		case 1://英文角色
-			startToDetail(1);
+		case 1:
+
 			break;
-		case 2://语速
-			
+		case 2:
+
 			break;
-		case 3://语调
-			
+		case 3:
+
 			break;
-		case 4://语音风格
-			startToDetail(4);
+		case 4:
+
 			break;
-		case 5://语音音效
-			startToDetail(5);
+		case 5:
+
 			break;
 		}
-	}
-	
-	private void startToDetail(int flag){
-		Intent intent = new Intent(this,MenuVoiceDetailActivity.class);
-		intent.putExtra("voice_flag", flag);
-		startActivity(intent);
 	}
 }
