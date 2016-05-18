b7f1cf77c9c5a65b33239e84775b905d5cd68ec7
wzp <wzp1029@126.com>
2016-05-18 10:39:33 +0800

    Daisy2.0格式正文解析。

---
+++ ebook/src/com/sunteam/ebook/entity/DiasySentenceNode.java
@@ -0,0 +1,22 @@
+package com.sunteam.ebook.entity;
+
+import java.io.Serializable;
+
+/**
+ * Diasy 句子节点类
+ * 
+ * @author wzp
+ * 
+ */
+public class DiasySentenceNode implements Serializable
+{	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7359722933553886963L;
+	
+	public String sentence;		//句子
+	public String audioFile;	//音频文件
+	public long startTime;		//开始时间
+	public long endTime;		//结束时间
+}
--- ebook/src/com/sunteam/ebook/util/DaisyFileReaderUtils.java
+++ ebook/src/com/sunteam/ebook/util/DaisyFileReaderUtils.java
@@ -5,7 +5,10 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import android.text.TextUtils;
+
 import com.sunteam.ebook.entity.DiasyNode;
+import com.sunteam.ebook.entity.DiasySentenceNode;
 
 /**
  * Daisy文件读取工具类。
@@ -16,14 +19,21 @@ public class DaisyFileReaderUtils
 {
 	private static final String TAG_BODY_START = "<body>";
 	private static final String TAG_BODY_END = "</body>";
+	
+	//以下TAG主要在ncc.html文件中使用
 	private static final String TAG_H_START = "<h";
 	private static final String TAG_H_END = "</h";
 	private static final String TAG_A_START = "<a href=\"";
 	private static final String TAG_A_END = "</a>";
 	
+	//以下TAG主要在*.smil文件中使用
+	private static final String TAG_TEXT_START = "<text";
+	private static final String TAG_TEXT_END = "/>";
+	private static final String TAG_AUDIO_START = "<audio";
+	private static final String TAG_AUDIO_END = "/>";
+	
 	private static DaisyFileReaderUtils instance = null;
 
-	private String mStrCharsetName = "utf-8";		//编码格式，默认为utf-8
 	private ArrayList<DiasyNode> mDiasyNodeList = new ArrayList<DiasyNode>();	//保存索引列表
 	
 	public static DaisyFileReaderUtils getInstance()
@@ -82,7 +92,7 @@ public class DaisyFileReaderUtils
 		try 
 		{
 			IdentifyEncoding ie = new IdentifyEncoding();
-			mStrCharsetName = ie.GetEncodingName( fullpath );	//得到文本编码
+			String strCharsetName = ie.GetEncodingName( fullpath );	//得到文本编码
 			
 			File file = new File(fullpath);
 			if( !file.exists() )
@@ -101,7 +111,7 @@ public class DaisyFileReaderUtils
 			fis.read(buffer);
 			fis.close();
 		
-			String data = new String(buffer, mStrCharsetName);
+			String data = new String(buffer, strCharsetName);
 			int start = data.indexOf(TAG_BODY_START);
 			int end = data.lastIndexOf(TAG_BODY_END);
 			
@@ -154,86 +164,7 @@ public class DaisyFileReaderUtils
 					}
 				}
 				
-				String[] splitUnicode = splitHref[1].split("&#");
-				if( (null == splitUnicode ) || ( 0 == splitUnicode.length) )
-				{
-					node.name = splitHref[1];					//节点名称
-				}
-				else
-				{
-					node.name = "";
-					for( int i = 0; i < splitUnicode.length; i++ )
-					{
-						if( "".equals(splitUnicode[i]) )
-						{
-							continue;
-						}
-						String ch = splitUnicode[i].substring(0, 1);
-						if( "x".equals(ch) || "X".equals(ch) )	//十六进制
-						{
-							int seq = splitUnicode[i].indexOf(";");
-							if( -1 == seq )
-							{
-								node.name = splitUnicode[i];
-								continue;
-							}
-							
-							String unicode = splitUnicode[i].substring(1, seq);
-							try
-							{
-								int code = Integer.parseInt(unicode, 16);
-								byte[] byteCode = new byte[2];
-								byteCode[0] = (byte) ((code&0x0000ff00)>>8);
-								byteCode[1] = (byte) (code&0x000000ff);
-								
-								node.name += new String(byteCode, "utf-16be");
-								
-								String temp = splitUnicode[i].substring(seq+1);
-								if( null != temp )
-								{
-									node.name += temp;
-								}
-							}
-							catch( Exception e )
-							{
-								e.printStackTrace();
-								node.name = splitUnicode[i];
-							}
-						}
-						else	//十进制
-						{
-							int seq = splitUnicode[i].indexOf(";");
-							if( -1 == seq )
-							{
-								node.name = splitUnicode[i];
-								continue;
-							}
-							
-							String unicode = splitUnicode[i].substring(0, seq);
-							try
-							{
-								int code = Integer.parseInt(unicode, 10);
-								byte[] byteCode = new byte[2];
-								byteCode[0] = (byte) ((code&0x0000ff00)>>8);
-								byteCode[1] = (byte) (code&0x000000ff);
-								
-								node.name += new String(byteCode, "utf-16be");
-								
-								String temp = splitUnicode[i].substring(seq+1);
-								if( null != temp )
-								{
-									node.name += temp;
-								}
-							}
-							catch( Exception e )
-							{
-								e.printStackTrace();
-								node.name = splitUnicode[i];
-							}
-						}
-					}
-				}
-				
+				node.name = getEscapeString(splitHref[1]);	//得到转义字符串
 				mDiasyNodeList.add(node);
 				
 				body = body.substring(oldEnd+TAG_H_END.length());
@@ -247,5 +178,335 @@ public class DaisyFileReaderUtils
 		{
 			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * 得到某个叶子节点的全部句子信息
+	 * @param smilPath：此叶子节点的句子所在的文件
+	 * 			lableName: 句子开始的标签(貌似无用)
+	 * @return 所有句子节点列表
+	*/	
+	public ArrayList<DiasySentenceNode> getDiasySentenceNodeList( String path, String smil, String lableName )
+	{		
+		ArrayList<DiasySentenceNode> list = new ArrayList<DiasySentenceNode>();
+		
+		try 
+		{
+			final String smilPath = path+"/"+smil;
+			
+			IdentifyEncoding ie = new IdentifyEncoding();
+			String strCharsetName = ie.GetEncodingName( smilPath );	//得到文本编码
+			
+			File file = new File(smilPath);
+			if( !file.exists() )
+			{
+				return	list;
+			}
+			int length = (int)file.length();
+			if( 0 == length )
+			{
+				return	list;
+			}
+			
+			//先将smil文件读取到内存
+			FileInputStream fis = new FileInputStream(file);
+			byte[] buffer = new byte[length];
+			fis.read(buffer);
+			fis.close();
+		
+			String data = new String(buffer, strCharsetName);
+			int start = data.indexOf(TAG_BODY_START);
+			int end = data.lastIndexOf(TAG_BODY_END);
+			
+			if( ( -1 == start ) || ( -1 == end ) )
+			{
+				//body为空
+				return	list;
+			}
+			
+			String body = data.substring(start+TAG_BODY_START.length(), end);	//得到body内容
+			
+			while( true )
+			{
+				start = body.indexOf(TAG_TEXT_START);
+				end = body.indexOf(TAG_TEXT_END);
+				if( ( -1 == start ) || ( -1 == end ) )
+				{
+					break;
+				}
+				
+				String txtItem = body.substring(start+TAG_TEXT_START.length(), end);	//取得一个txt item
+				String[] splitTextItem = txtItem.split(" ");
+				if( ( null == splitTextItem ) || ( 0 == splitTextItem.length ) )
+				{
+					break;
+				}
+				
+				int i = 0;
+				for( ; i < splitTextItem.length; i++ )
+				{
+					if( splitTextItem[i].indexOf("src=\"") == 0 )
+					{
+						break;
+					}
+				}
+				
+				body = body.substring(end+TAG_TEXT_END.length());
+				
+				if( i >= splitTextItem.length )
+				{
+					continue;
+				}
+				
+				String temp = splitTextItem[i].replaceAll("\"", "#");
+				if( TextUtils.isEmpty(temp) )
+				{
+					continue;
+				}
+				
+				String[] splitText = temp.split("#");
+				if( ( null == splitText ) || ( splitText.length < 3 ) )
+				{
+					continue;
+				}
+				
+				DiasySentenceNode node = new DiasySentenceNode();
+				node.sentence = getEscapeString(getDaisySentence( path, splitText[1], splitText[2] ));
+				body = getDaisySentenceAudioInfo(body, node);
+				
+				list.add(node);
+			}
+		} 
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		
+		return	list;
+	}
+	
+	/**
+	 * 得到句子内容
+	 * @param sentencePath：句子内容所在的文件
+	 * 			lableName: 句子所在的标签
+	 * @return 句子内容
+	*/	
+	private String getDaisySentence( String path, String sentenceHref, String lableName )
+	{
+		try 
+		{
+			final String sentencePath = path + "/" + sentenceHref;
+			
+			IdentifyEncoding ie = new IdentifyEncoding();
+			String strCharsetName = ie.GetEncodingName( sentencePath );	//得到文本编码
+			
+			File file = new File(sentencePath);
+			if( !file.exists() )
+			{
+				return	"";
+			}
+			int length = (int)file.length();
+			if( 0 == length )
+			{
+				return	"";
+			}
+			
+			//先将content文件读取到内存
+			FileInputStream fis = new FileInputStream(file);
+			byte[] buffer = new byte[length];
+			fis.read(buffer);
+			fis.close();
+		
+			String data = new String(buffer, strCharsetName);
+			int start = data.indexOf(TAG_BODY_START);
+			int end = data.lastIndexOf(TAG_BODY_END);
+			
+			if( ( -1 == start ) || ( -1 == end ) )
+			{
+				//body为空
+				return	"";
+			}
+			
+			String body = data.substring(start+TAG_BODY_START.length(), end);	//得到body内容
+			
+			while( true )
+			{
+				start = body.indexOf(lableName);
+				if( -1 == start )
+				{
+					return	"";
+				}
+				
+				body = body.substring(start+lableName.length());
+				
+				start = body.indexOf(TAG_A_START);
+				end = body.indexOf(TAG_A_END);
+				if( ( -1 == start ) || ( -1 == end ) )
+				{
+					return	"";
+				}
+				
+				String sentenceItem = body.substring(start+TAG_A_START.length(), end);	//取得一个sentence item
+				String[] splitSentenceItem = sentenceItem.split(">");
+				if( ( null == splitSentenceItem ) || ( 2 != splitSentenceItem.length ) )
+				{
+					return	"";
+				}
+				
+				return	splitSentenceItem[1];
+			}
+		} 
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+
+		return	"";
+	}
+	
+	//得到句子对应的音频文件信息
+	private String getDaisySentenceAudioInfo( String body, DiasySentenceNode node )
+	{
+		int n = 0;
+		while( true )
+		{
+			int textStart = body.indexOf(TAG_TEXT_START);
+			int start = body.indexOf(TAG_AUDIO_START);
+			int end = body.indexOf(TAG_AUDIO_END);
+			if( ( -1 == start ) || ( -1 == end ) || ( textStart != -1 && start > textStart ) )
+			{
+				break;
+			}
+			
+			String audioItem = body.substring(start+TAG_AUDIO_START.length(), end);	//取得一个audio item
+			String[] splitAudioItem = audioItem.split(" ");
+			if( ( null == splitAudioItem ) || ( 0 == splitAudioItem.length ) )
+			{
+				break;
+			}
+			
+			int i = 0;
+			for( ; i < splitAudioItem.length; i++ )
+			{
+				if( splitAudioItem[i].indexOf("src=\"") == 0 )	//音频文件
+				{
+					String[] splitAudio = splitAudioItem[i].replaceAll("\"", "#").split("#");
+					node.audioFile = splitAudio[1];
+				}
+				else if( splitAudioItem[i].indexOf("clip-begin=\"npt=") == 0 )	//时间开始
+				{
+					if( 0 == n )
+					{
+						String[] splitTime = splitAudioItem[i].replaceAll("s\"", "").split("=");
+						node.startTime = (long)(Float.parseFloat(splitTime[2])*1000);
+					}
+					n++;
+				}
+				else if( splitAudioItem[i].indexOf("clip-end=\"npt=") == 0 )	//时间结束
+				{
+					String[] splitTime = splitAudioItem[i].replaceAll("s\"", "").split("=");
+					node.endTime = (long)(Float.parseFloat(splitTime[2])*1000);
+				}
+			}
+			
+			body = body.substring(end+TAG_AUDIO_END.length());
+		}
+		
+		return	body;
+	}
+	
+	//得到转义字符串
+	private String getEscapeString( String str )
+	{
+		String name = "";
+		String[] splitUnicode = str.split("&#");
+		
+		if( (null == splitUnicode ) || ( 0 == splitUnicode.length) )
+		{
+			return	str;					//节点名称
+		}
+		else
+		{
+			for( int i = 0; i < splitUnicode.length; i++ )
+			{
+				if( "".equals(splitUnicode[i]) )
+				{
+					continue;
+				}
+				String ch = splitUnicode[i].substring(0, 1);
+				if( "x".equals(ch) || "X".equals(ch) )	//十六进制
+				{
+					int seq = splitUnicode[i].indexOf(";");
+					if( -1 == seq )
+					{
+						name = splitUnicode[i];
+						continue;
+					}
+					
+					String unicode = splitUnicode[i].substring(1, seq);
+					try
+					{
+						int code = Integer.parseInt(unicode, 16);
+						byte[] byteCode = new byte[2];
+						byteCode[0] = (byte) ((code&0x0000ff00)>>8);
+						byteCode[1] = (byte) (code&0x000000ff);
+						
+						name += new String(byteCode, "utf-16be");
+						
+						String temp = splitUnicode[i].substring(seq+1);
+						if( null != temp )
+						{
+							name += temp;
+						}
+					}
+					catch( Exception e )
+					{
+						e.printStackTrace();
+						name = splitUnicode[i];
+					}
+				}
+				else	//十进制
+				{
+					int seq = splitUnicode[i].indexOf(";");
+					if( -1 == seq )
+					{
+						name = splitUnicode[i];
+						continue;
+					}
+					
+					String unicode = splitUnicode[i].substring(0, seq);
+					try
+					{
+						int code = Integer.parseInt(unicode, 10);
+						byte[] byteCode = new byte[2];
+						byteCode[0] = (byte) ((code&0x0000ff00)>>8);
+						byteCode[1] = (byte) (code&0x000000ff);
+						
+						name += new String(byteCode, "utf-16be");
+						
+						String temp = splitUnicode[i].substring(seq+1);
+						if( null != temp )
+						{
+							name += temp;
+						}
+					}
+					catch( Exception e )
+					{
+						e.printStackTrace();
+						name = splitUnicode[i];
+					}
+				}
+			}
+		}
+		
+		return	name;
 	}
 }
