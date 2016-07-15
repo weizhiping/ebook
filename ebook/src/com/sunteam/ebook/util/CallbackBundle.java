6cdc4172b80c680f0d30c729bb8311b94fbd0728
wzp <wzp1029@126.com>
2016-07-15 10:29:14 +0800

    增加回调接口。

---
+++ ebook/src/com/sunteam/ebook/util/CallbackBundle.java
@@ -0,0 +1,15 @@
+package com.sunteam.ebook.util;
+
+import java.util.List;
+
+import android.os.Bundle;
+
+/**
+ * 简单的Bundle参数回调接口
+ * 
+ * @author wzp
+ */
+public interface CallbackBundle
+{
+	abstract void callback( Bundle bundle );
+}
