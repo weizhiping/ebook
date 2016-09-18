858c5c2e3e01442eaebdf131a06ef020ab292beb
sylar <loujiawen924@163.com>
2016-09-18 11:31:30 +0800

    解决功能菜单中北京音乐及书签管理问题

--- ebook/src/com/sunteam/ebook/MenuMusicDetailActivity.java
+++ ebook/src/com/sunteam/ebook/MenuMusicDetailActivity.java
@@ -8,7 +8,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.widget.FrameLayout;
@@ -18,6 +17,7 @@ import com.sunteam.ebook.entity.FileInfo;
 import com.sunteam.ebook.entity.ScreenManager;
 import com.sunteam.ebook.util.EbookConstants;
 import com.sunteam.ebook.util.FileOperateUtils;
+import com.sunteam.ebook.util.PublicUtils;
 import com.sunteam.ebook.view.MainView;
 
 /**
@@ -25,12 +25,13 @@ import com.sunteam.ebook.view.MainView;
  * 
  * @author sylar
  */
-public class MenuMusicDetailActivity extends Activity implements OnEnterListener {
+public class MenuMusicDetailActivity extends Activity implements
+		OnEnterListener {
 	private FrameLayout mFlContainer = null;
 	private MainView mMainView = null;
 	private ArrayList<String> mMenuList = null;
 	private ArrayList<FileInfo> fileList = null;
-	private int flag;//0开关 1 音乐选择
+	private int flag;// 0开关 1 音乐选择
 	private SharedPreferences shared;
 
 	@Override
@@ -38,7 +39,8 @@ public class MenuMusicDetailActivity extends Activity implements OnEnterListener
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		ScreenManager.getScreenManager().pushActivity(this);
-		shared = getSharedPreferences(EbookConstants.SETTINGS_TABLE,Context.MODE_PRIVATE);
+		shared = getSharedPreferences(EbookConstants.SETTINGS_TABLE,
+				Context.MODE_PRIVATE);
 		flag = getIntent().getIntExtra("music_flag", 0);
 		fileList = new ArrayList<FileInfo>();
 		mMenuList = new ArrayList<String>();
@@ -46,15 +48,9 @@ public class MenuMusicDetailActivity extends Activity implements OnEnterListener
 	}
 
 	private void initViews() {
-		Resources res = getResources();
-		String[] menus = null;
-		if(0 == flag){
-			menus = res.getStringArray(R.array.array_menu_music_on);
-			int length = menus.length;
-			for (int i = 0; i < length; i++) {
-				mMenuList.add(menus[i]);
-			}
-		}else{
+		if (0 == flag) {
+			initSwitch();
+		} else {
 			initFiles();
 		}
 		String title = getIntent().getStringExtra("title");
@@ -62,71 +58,87 @@ public class MenuMusicDetailActivity extends Activity implements OnEnterListener
 		mMainView = new MainView(this, this, title, mMenuList);
 		mFlContainer.removeAllViews();
 		mFlContainer.addView(mMainView.getView());
+		if(0 == flag){
+			boolean isOpen = shared.getBoolean(EbookConstants.MUSICE_STATE, false);
+			if(isOpen){
+				mMainView.setSelection(0);
+			}else{
+				mMainView.setSelection(1);
+			}
+		}
 	}
-    
-    @Override
-    public void onPause()
-    {
-    	if( mMainView != null )
-    	{
-    		mMainView.onPause();
-    	}
-    	super.onPause();
-    }
-    
-    @Override
-    public void onResume()
-    {
-    	if( mMainView != null )
-    	{
-    		mMainView.onResume();
-    	}
-    	super.onResume();
-    }
- 
-    @Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) 
-	{
+
+	@Override
+	public void onPause() {
+		if (mMainView != null) {
+			mMainView.onPause();
+		}
+		super.onPause();
+	}
+
+	@Override
+	public void onResume() {
+		if (mMainView != null) {
+			mMainView.onResume();
+		}
+		super.onResume();
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		return mMainView.onKeyDown(keyCode, event);
 	}
-	
+
 	@Override
-	public boolean onKeyUp(int keyCode, KeyEvent event) 
-	{
-		return	mMainView.onKeyUp(keyCode, event);
+	public boolean onKeyUp(int keyCode, KeyEvent event) {
+		return mMainView.onKeyUp(keyCode, event);
 	}
 
 	@Override
 	public void onEnterCompleted(int selectItem, String menu, boolean isAuto) {
 		Intent intent = new Intent(EbookConstants.MENU_PAGE_EDIT);
 		Editor edit = shared.edit();
-		if(0 == flag){
+		if (0 == flag) {
 			intent.putExtra("result_flag", 1);
-			if(0 == selectItem){
+			if (0 == selectItem) {
 				edit.putBoolean(EbookConstants.MUSICE_STATE, true);
-			}else{
+			} else {
 				edit.putBoolean(EbookConstants.MUSICE_STATE, false);
 			}
-		}else{
+		} else {
 			intent.putExtra("result_flag", 2);
-			edit.putString(EbookConstants.MUSICE_PATH, fileList.get(selectItem).path);
+			edit.putString(EbookConstants.MUSICE_PATH,
+					fileList.get(selectItem).path);
 		}
 		edit.commit();
 		sendBroadcast(intent);
+		PublicUtils.showToast(MenuMusicDetailActivity.this, getResources().getString(R.string.setting_success));
 		ScreenManager.getScreenManager().popAllActivityExceptOne();
 	}
-	
-	// 初始化显示文件
-		private void initFiles() {
-			ArrayList<File> filesList = FileOperateUtils.getMusicInDir();
-			if (null != filesList) {
-				for (File f: filesList) {
-					FileInfo info = new FileInfo();
-					info.name = f.getName();
-					info.path = f.getPath();
-					mMenuList.add(info.name);
-					fileList.add(info);
-				}
-			}
+
+	// 初始化开关
+	private void initSwitch() {
+		String[] menus = getResources().getStringArray(R.array.array_menu_music_on);
+		int length = menus.length;
+		for (int i = 0; i < length; i++) {
+			mMenuList.add(menus[i]);
 		}
+	}
+
+	// 初始化显示文件
+	private void initFiles() {
+		ArrayList<File> filesList = FileOperateUtils.getMusicInDir();
+		if (null != filesList && 0 < filesList.size()) {
+			for (File f : filesList) {
+				FileInfo info = new FileInfo();
+				info.name = f.getName();
+				info.path = f.getPath();
+				mMenuList.add(info.name);
+				fileList.add(info);
+			}
+		}else{
+			PublicUtils.showToast(this, getResources().getString(R.string.menu_muisc_null));
+			ScreenManager.getScreenManager().popAllActivityExceptOne();
+		}
+	}
 }
--- ebook/src/com/sunteam/ebook/MenuTextEditActivity.java
+++ ebook/src/com/sunteam/ebook/MenuTextEditActivity.java
@@ -3,12 +3,10 @@ package com.sunteam.ebook;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnKeyListener;
-import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -26,9 +24,10 @@ import com.sunteam.ebook.util.TTSUtils;
  * @author sylar
  */
 public class MenuTextEditActivity extends Activity {
-	private EditText numView;
+	private TextView numView;
 	private DatabaseManager manager;
 	private FileInfo info;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -46,55 +45,89 @@ public class MenuTextEditActivity extends Activity {
 		info = (FileInfo) intent.getSerializableExtra("fileinfo");
 		RelativeLayout layout = (RelativeLayout) findViewById(R.id.menu_layout);
 		TextView titleView = (TextView) findViewById(R.id.title_menu);
-		numView = (EditText) findViewById(R.id.text_edit);
+		numView = (TextView) findViewById(R.id.text_mark);
 		View line = findViewById(R.id.menu_line);
 		titleView.setText(title);
-		
+
 		Tools tools = new Tools(this);
-		
-		layout.setBackgroundColor(tools.getBackgroundColor());	// 设置View的背景色
-		titleView.setTextColor(tools.getFontColor()); 			// 设置title的背景色
-		line.setBackgroundColor(tools.getFontColor());			// 设置分割线的背景色
+
+		layout.setBackgroundColor(tools.getBackgroundColor()); // 设置View的背景色
+		titleView.setTextColor(tools.getFontColor()); // 设置title的背景色
+		line.setBackgroundColor(tools.getFontColor()); // 设置分割线的背景色
 		numView.setTextColor(tools.getFontColor());
-		
-		final float scale = this.getResources().getDisplayMetrics().density/0.75f;	//计算相对于ldpi的倍数;
+
+		final float scale = this.getResources().getDisplayMetrics().density / 0.75f; // 计算相对于ldpi的倍数;
 		float fontSize = tools.getFontSize() * scale;
-		titleView.setTextSize(TypedValue.COMPLEX_UNIT_PX, fontSize-2*EbookConstants.LINE_SPACE*scale); // 设置title字号
-		numView.setTextSize(TypedValue.COMPLEX_UNIT_PX, fontSize-2*EbookConstants.LINE_SPACE*scale);		
-		titleView.setHeight((int)fontSize); // 设置控件高度
-		
-		if(null == info.diasyPath){
-			numView.setText(String.format(getResources().getString(R.string.menu_text_tips), currentPage)+" " + currentText);
-		}else{
-			numView.setText(currentText + " " + String.format(getResources().getString(R.string.menu_text_tips), currentPage));
+		titleView.setTextSize(TypedValue.COMPLEX_UNIT_PX, fontSize - 2
+				* EbookConstants.LINE_SPACE * scale); // 设置title字号
+		numView.setTextSize(TypedValue.COMPLEX_UNIT_PX, fontSize - 2
+				* EbookConstants.LINE_SPACE * scale);
+		titleView.setHeight((int) fontSize); // 设置控件高度
+		if (null == info.diasyPath) {
+			numView.setText(String.format(
+					getResources().getString(R.string.menu_text_tips),
+					currentPage) + " " + currentText);
+		} else {
+			numView.setText(currentText
+					+ " "
+					+ String.format(
+							getResources().getString(R.string.menu_text_tips),
+							currentPage));
 		}
-		TTSUtils.getInstance().speakMenu(title + "，" + numView.getText().toString());
-		numView.setOnKeyListener(new OnKeyListener() {
-			
-			@Override
-			public boolean onKey(View v, int keyCode, KeyEvent event) {
-				if(keyCode == KeyEvent.KEYCODE_DPAD_CENTER && event.getAction() == KeyEvent.ACTION_DOWN){
-					if(!TextUtils.isEmpty(numView.getText())){
-						info.name = numView.getText().toString();
-						boolean hasMark = manager.insertMarkToDb(info);
-						if(!hasMark){
-							PublicUtils.showToast(MenuTextEditActivity.this
-									, getResources().getString(R.string.add_mark_su));
-						}else{
-							PublicUtils.showToast(MenuTextEditActivity.this
-									, getResources().getString(R.string.add_mark_has));
-						}
-					}
-					ScreenManager.getScreenManager().popAllActivityExceptOne();
-				}
-				return false;
-			}
-		});
+		TTSUtils.getInstance().speakMenu(
+				title + "，" + numView.getText().toString());
+
+		// numView.setOnKeyListener(new OnKeyListener() {
+		//
+		// @Override
+		// public boolean onKey(View v, int keyCode, KeyEvent event) {
+		// if(keyCode == KeyEvent.KEYCODE_DPAD_CENTER && event.getAction() ==
+		// KeyEvent.ACTION_DOWN){
+		// if(!TextUtils.isEmpty(numView.getText())){
+		// info.name = numView.getText().toString();
+		// boolean hasMark = manager.insertMarkToDb(info);
+		// if(!hasMark){
+		// PublicUtils.showToast(MenuTextEditActivity.this
+		// , getResources().getString(R.string.add_mark_su));
+		// }else{
+		// PublicUtils.showToast(MenuTextEditActivity.this
+		// , getResources().getString(R.string.add_mark_has));
+		// }
+		// }
+		// ScreenManager.getScreenManager().popAllActivityExceptOne();
+		// }
+		// return false;
+		// }
+		// });
+
 	}
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
-		
+
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		switch (keyCode) {
+		case KeyEvent.KEYCODE_BACK:// 返回
+			break;
+		case KeyEvent.KEYCODE_DPAD_CENTER: // 确定
+		case KeyEvent.KEYCODE_ENTER:
+			info.name = numView.getText().toString();
+			Log.e("centre", "------info name---:" + info.name);
+			boolean hasMark = manager.insertMarkToDb(info);
+			if (!hasMark) {
+				PublicUtils.showToast(MenuTextEditActivity.this, getResources()
+						.getString(R.string.add_mark_su));
+			} else {
+				PublicUtils.showToast(MenuTextEditActivity.this, getResources()
+						.getString(R.string.add_mark_has));
+			}
+			ScreenManager.getScreenManager().popAllActivityExceptOne();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
 	}
 }
