b1172b9d431de60e657213fbd557ef75511db09d
wzp <wzp1029@126.com>
2017-03-24 09:52:22 +0800

    改变背景音乐音量模式。

--- ebook/src/com/sunteam/ebook/util/MediaPlayerUtils.java
+++ ebook/src/com/sunteam/ebook/util/MediaPlayerUtils.java
@@ -266,6 +266,7 @@ public class MediaPlayerUtils
 			try 
 			{
 				mMediaPlayer.reset();
+				mMediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
 				mMediaPlayer.setDataSource(audioPath);
 				mMediaPlayer.prepare();
 				mMediaPlayer.setLooping(true);
